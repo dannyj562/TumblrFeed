b16476c4b3fcaaebcd69119883347ba7c30cc5be
dannyj562 <riveradanny51@gmail.com>
2018-09-14 12:15:42 -0700

    Add Podfile and Tumblr Feed Implementation

---
+++ TumblrFeed/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Danny on 9/14/18.
+//  Copyright © 2018 Danny Rivera. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
